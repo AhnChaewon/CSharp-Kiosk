b7f692bdffa2987536b51562a0c3f7705b4d414b
GyuSeok Nam <52943549+nks0614@users.noreply.github.com>
2020-09-22 13:54:46 +0900

    [DELETE] Window

---
+++ CSharp_Kiosk/CSharp_Kiosk/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace CSharp_Kiosk
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Click(object sender, RoutedEventArgs e)
+        {
+            ucOrder.Visibility = Visibility.Visible;
+        }
+    }
+}
