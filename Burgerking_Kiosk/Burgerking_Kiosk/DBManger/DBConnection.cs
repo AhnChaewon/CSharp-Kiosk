bf00b1b987da206259de210d7623301c7afbd72a
GyuSeok Nam <52943549+nks0614@users.noreply.github.com>
2020-11-04 12:04:45 +0900

    [ADD] DBManger files

---
+++ Burgerking_Kiosk/Burgerking_Kiosk/DBManger/DBConnection.cs
@@ -0,0 +1,45 @@
+﻿using MySql.Data.MySqlClient;
+using Renci.SshNet.Security.Cryptography;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Burgerking_Kiosk.DBManger
+{
+    class DBConnection
+    {
+
+        private MySqlConnection connection = null;
+        private MySqlCommand cmd = null;
+
+        public void setCommand(string sql)
+        {
+            cmd = new MySqlCommand(sql, connection);
+        }
+
+        public void connectDB()
+        {
+            String DBConnectionPW = DBPassword.connStr;
+            connection = new MySqlConnection(DBConnectionPW);
+            connection.Open();
+        }
+
+        public void closeConnection()
+        {
+            connection.Close();
+        }
+
+        public int execute()
+        {
+            return cmd.ExecuteNonQuery();
+        }
+
+        public MySqlDataReader executeReadQuery()
+        {
+            return cmd.ExecuteReader();
+        }
+
+    }
+}
